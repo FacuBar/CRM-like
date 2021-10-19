4e10659539525aaa650a578e1ba74ab069036365
FacuBar <efacubar@gmail.com>
2021-10-19 18:16:38 -0300

    fixing dotenv path referencing

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname + '/../config.env') });
 const app = require('./app');
-
-dotenv.config({ path: './config.env' });
 
 mongoose
   .connect(process.env.DATABASE, {
