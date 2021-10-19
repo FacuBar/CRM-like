bc13020bf9c0044565cb4f25fa66eaa893cc1a53
FacuBar <efacubar@gmail.com>
2021-10-19 19:16:50 -0300

    implementing CRUD funcs and pagination

---
+++ src/controllers/commentController.js
@@ -0,0 +1,32 @@
+const Comment = require('../models/commentModel');
+const wrapAsync = require('../utils/wrapAsync');
+const { sendMsg } = require('./wspController');
+
+// Not required
+// exports.getAll = wrapAsync(async (req, res) => {
+//   let comments = await req.RepairRequest.populate('comments');
+//   comments = comments.comments;
+
+//   res.status(200).json({
+//     status: 'success',
+//     comments,
+//   });
+// });
+
+exports.createNew = wrapAsync(async (req, res) => {
+  const newComment = await Comment.create({
+    text: req.body.text,
+  });
+
+  req.RepairRequest.comments.push(newComment._id);
+  req.RepairRequest.save();
+
+  if (req.body.text.split(' ')[0] === ':enviar:') {
+    sendMsg(req);
+  }
+
+  res.status(200).json({
+    status: 'success',
+    newComment,
+  });
+});
---
+++ src/controllers/repairRequestController.js
@@ -0,0 +1,82 @@
+const RepairRequest = require('../models/repairRequestModel');
+const wrapAsync = require('../utils/wrapAsync');
+
+// '/repairs/:id'
+
+exports.getAll = wrapAsync(async (req, res) => {
+  let searchQuery = {};
+  if (req.query.search) searchQuery = { $text: { $search: req.query.search } };
+
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields', 'search'];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  const currentPage = req.query.page ? req.query.page : 1;
+
+  const documents = await RepairRequest.paginate(
+    { ...searchQuery, ...queryObj },
+    { page: currentPage, limit: 5, sort: { statusId: 1, sentAt: -1 } }
+  );
+
+  /**
+   * @todo
+   * replace json response for view rendering.
+   */
+  if (documents)
+    res.status(200).json({
+      status: 'success',
+      repairRequests: documents.docs,
+      totalPages: documents.pages,
+      currentPage: documents.page,
+    });
+  else
+    res.status(404).json({
+      status: 'fail',
+      message: 'Not found',
+    });
+});
+
+exports.createNew = wrapAsync(async (req, res) => {
+  const data = {
+    contactInfo: {
+      name: req.body.name,
+      phone: req.body.phone,
+      address: req.body.address,
+    },
+    issuedBy: req.body.issuedBy,
+    modelo: req.body.modelo,
+    nserie: req.body.nserie,
+    text: req.body.text,
+  };
+
+  const repairRequest = await RepairRequest.create(data);
+
+  // repairingEmail(repairRequest);
+
+  res.status(200).json({
+    status: 'success',
+    repairRequest,
+  });
+});
+
+// '/repairs/:id'
+
+exports.findRepairRequest = wrapAsync(async (req, _, next) => {
+  const repairRequest = await RepairRequest.findById(req.params.id);
+
+  // TODO: replace for 404 page
+  if (!repairRequest) return next(new AppError('RepairRequest not found', 404));
+
+  req.repairRequest = repairRequest;
+  next();
+});
+
+exports.getOne = wrapAsync(async (req, res) => {
+  res.json(req.repairRequest);
+});
+
+exports.modifyStatus = wrapAsync(async (req, res) => {
+  req.repairRequest.status = req.body.status;
+  await req.repairRequest.save();
+  res.json(req.repairRequest);
+});
